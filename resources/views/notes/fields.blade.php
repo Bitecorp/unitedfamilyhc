7204645067e51bfac90df7b97fcfbfb66bbf62a7
Pedro Hernandez <phernandez@initiumsoft.com>
2022-07-07 06:23:42 -0500

    add firma a las notas

--- resources/views/notes/fields.blade.php
+++ resources/views/notes/fields.blade.php
@@ -57,7 +57,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-xl">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Signature</h5>
@@ -66,7 +66,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                <canvas id="signature" style="min-height: 800px !important; max-width: 897px !important; padding: 0 !important; margin: 0 auto !important;"></canvas>
+                <canvas id="signature" style="min-height: 800px !important; max-width: 1500px !important; padding: 0 !important; margin: 0 auto !important;"></canvas>
             </div>
             <div class="modal-footer">
                 <button id="btn-clear-signature" type="button" class="btn btn-secondary" data-dismiss="modal">Clear</button>
