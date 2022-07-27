430e16a3adf57900752a540c61ea0ea9643e4ca5
ingpedro1991 <ing.pedro.h@gmail.com>
2022-07-26 21:54:33 -0400

    cambio controller alerts

--- app/Http/Controllers/AlertDocumentsController.php
+++ app/Http/Controllers/AlertDocumentsController.php
@@ -78,7 +78,9 @@ class AlertDocumentsController extends AppBaseController
         }
 
         foreach($dataDocuments AS $key => $dataDocument){
-            $dataUser = User::find($dataDocument['user_id']);
+            if(isset($dataDocument) && !empty($dataDocument)){
+                $dataUser = User::find($dataDocument['user_id']);
+            }
             if(isset($dataUser) && !empty($dataUser)){
                 array_push($workers, $dataUser);
             }
