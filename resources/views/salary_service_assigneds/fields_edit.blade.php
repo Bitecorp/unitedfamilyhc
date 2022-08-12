09533984f318284a12e9373ff98c3223c3f28d64
Pedro Hernandez <phernandez@initiumsoft.com>
2022-08-11 20:50:46 -0500

    cambio de inputs

--- resources/views/salary_service_assigneds/fields_edit.blade.php
+++ resources/views/salary_service_assigneds/fields_edit.blade.php
@@ -77,19 +77,6 @@
         </div>
     </div>
 
-    <div class="col">
-        <!-- unit Id Field -->
-        <div class="form-group">
-            {!! Form::label('unit_id', 'Unit Of Time:') !!}
-            <select name='unit_id' id='unit_id' class="form-control">
-                <option value='' selected>Select Unit of Time..</option>
-                @foreach($units as $unit)
-                    <option value='{{ $unit->id }}' {{ isset($config) && !empty($config) && isset($config->unit_id) && !empty($config->unit_id) && $config->unit_id == $unit->id ? 'selected' : '' }} >{{ $unit->time }} {{ $unit->type_unidad == 0 ? 'Minutes' : 'Hour'}}</option>
-                @endforeach
-            </select>
-        </div>
-    </div>
-
     <div class="col" {{ strpos(URL::previous(), "workers") ? 'hidden' : ''}}>
         <div class="form-group">
             {!! Form::label('code_patiente', 'Code Patient:') !!}
