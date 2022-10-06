4de0dc6c95a9e443084a5d89694c302749d20641
Pedro Hernandez <phernandez@initiumsoft.com>
2022-10-06 00:17:44 -0500

    correccion al documentEditor

--- resources/views/companies/show_fields.blade.php
+++ resources/views/companies/show_fields.blade.php
@@ -1,60 +1,60 @@
 <!-- Name Field -->
 <div class="form-group">
     {!! Form::label('name', 'Name:') !!}
-    <p>{{ $companies->name }}</p>
+    <p>{{ isset($companies) && !empty($companies) ? $companies->name : ''}}</p>
 </div>
 
 <!-- Street Addres Field -->
 <div class="form-group">
     {!! Form::label('street_addres', 'Street Addres:') !!}
-    <p>{{ $companies->street_addres }}</p>
+    <p>{{ isset($companies) && !empty($companies) ? $companies->street_addres : '' }}</p>
 </div>
 
 <!-- Apartment Unit Field -->
 <div class="form-group">
     {!! Form::label('apartment_unit', 'Apartment Unit:') !!}
-    <p>{{ $companies->apartment_unit }}</p>
+    <p>{{ isset($companies) && !empty($companies) ? $companies->apartment_unit : '' }}</p>
 </div>
 
 <!-- City Field -->
 <div class="form-group">
     {!! Form::label('city', 'City:') !!}
-    <p>{{ $companies->city }}</p>
+    <p>{{ isset($companies) && !empty($companies) ? $companies->city : '' }}</p>
 </div>
 
 <!-- State Field -->
 <div class="form-group">
     {!! Form::label('state', 'State:') !!}
-    <p>{{ $companies->state }}</p>
+    <p>{{ isset($companies) && !empty($companies) ? $companies->state : '' }}</p>
 </div>
 
 <!-- Zip Code Field -->
 <div class="form-group">
     {!! Form::label('zip_code', 'Zip Code:') !!}
-    <p>{{ $companies->zip_code }}</p>
+    <p>{{ isset($companies) && !empty($companies) ? $companies->zip_code : '' }}</p>
 </div>
 
 <!-- Home Phone Field -->
 <div class="form-group">
     {!! Form::label('home_phone', 'Home Phone:') !!}
-    <p>{{ $companies->home_phone }}</p>
+    <p>{{ isset($companies) && !empty($companies) ? $companies->home_phone : '' }}</p>
 </div>
 
 <!-- Alternate Phone Field -->
 <div class="form-group">
     {!! Form::label('alternate_phone', 'Alternate Phone:') !!}
-    <p>{{ $companies->alternate_phone }}</p>
+    <p>{{ isset($companies) && !empty($companies) ? $companies->alternate_phone : '' }}</p>
 </div>
 
 <!-- Ssn Field -->
 <div class="form-group">
     {!! Form::label('ssn', 'Ssn:') !!}
-    <p>{{ $companies->ssn }}</p>
+    <p>{{ isset($companies) && !empty($companies) ? $companies->ssn : '' }}</p>
 </div>
 
 <!-- Email Field -->
 <div class="form-group">
     {!! Form::label('email', 'Email:') !!}
-    <p>{{ $companies->email }}</p>
+    <p>{{ isset($companies) && !empty($companies) ? $companies->email : '' }}</p>
 </div>
 
