8bb02a794ed52a18be2b18fda237b4e8861797b4
Pedro Hernandez <phernandez@initiumsoft.com>
2022-10-06 00:41:26 -0500

    correccion al documentEditor

--- resources/views/confirmation_independents/show_fields.blade.php
+++ resources/views/confirmation_independents/show_fields.blade.php
@@ -60,8 +60,101 @@
 </br>
 
 @if($confirmationIndependent->independent_contractor == 1 && $confirmationIndependent->personalEmpresa == 2)
-<p>{{ $companies }}</p>
-    
+<p>{{ $companies->name }}</p>
+    <div class="container-fluid">
+        <div class="animated fadeIn">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">
+                            <i class="fa fa-plus-square-o fa-lg"></i>
+                            <strong>Compani</strong>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <!-- First Name Field -->
+                                    <div class="form-group">
+                                        {!! Form::label('name', 'Name:') !!}
+                                        <input type="text" name="name" class="form-control" readonly value="{{ isset($companies) && !empty($companies) ? $companies->name : '' }}" >
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <!-- Mi Field -->
+                                    <div class="form-group">
+                                        {!! Form::label('street_addres', 'Street Addres:') !!}
+                                        <input type="text" name="street_addres" class="form-control" readonly value="{{ isset($companies) && !empty($companies) ? $companies->street_addres : '' }}" >
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <!-- Last Name Field -->
+                                    <div class="form-group">
+                                        {!! Form::label('apartment_unit', 'Apartment Unit:') !!}
+                                        <input type="text" name="apartment_unit " class="form-control" readonly value="{{ isset($companies) && !empty($companies) ? $companies->apartment_unit : '' }}" >
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col">
+                                    <!-- First Name Field -->
+                                    <div class="form-group">
+                                        {!! Form::label('city', 'City:') !!}
+                                        <input type="text" name="city" class="form-control" readonly value="{{ isset($companies) && !empty($companies) ? $companies->city : '' }}" >
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <!-- Mi Field -->
+                                    <div class="form-group">
+                                        {!! Form::label('state', 'State:') !!}
+                                        <input type="text" name="state" class="form-control" readonly value="{{ isset($companies) && !empty($companies) ? $companies->state : '' }}" >
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <!-- Last Name Field -->
+                                    <div class="form-group">
+                                        {!! Form::label('zip_code', 'Zip Code:') !!}
+                                        <input type="text" name="zip_code " class="form-control" readonly value="{{ isset($companies) && !empty($companies) ? $companies->zip_code : '' }}" >
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col">
+                                    <!-- First Name Field -->
+                                    <div class="form-group">
+                                        {!! Form::label('home_phone', 'Home Phone:') !!}
+                                        <input type="text" name="chome_phone" class="form-control" readonly value="{{ isset($companies) && !empty($companies) ? $companies->home_phone : '' }}" >
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <!-- Mi Field -->
+                                    <div class="form-group">
+                                        {!! Form::label('alternate_phone', 'Alternate Phone:') !!}
+                                        <input type="text" name="alternate_phone" class="form-control" readonly value="{{ isset($companies) && !empty($companies) ? $companies->alternate_phone : '' }}" >
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <!-- Last Name Field -->
+                                    <div class="form-group">
+                                        {!! Form::label('ssn', 'SSN:') !!}
+                                        <input type="text" name="ssn " class="form-control" readonly value="{{ isset($companies) && !empty($companies) ? $companies->ssn : '' }}" >
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <!-- Last Name Field -->
+                                    <div class="form-group">
+                                        {!! Form::label('email', 'Email:') !!}
+                                        <input type="email" name="email " class="form-control" readonly value="{{ isset($companies) && !empty($companies) ? $companies->email : '' }}" >
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endif
 
 
